f36d5e0d7d89d1f1c61d8fbc59f5d9554e524acb
Abdul-Rahman Wunpini <55752850+ramz04@users.noreply.github.com>
2023-05-06 21:19:42 +0000

    Adding the Navbar Mobile Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,69 @@
 import img_mobile1 from './assets/images/mobile-image-hero-1.jpg'
+import img_mobile2 from './assets/images/mobile-image-hero-2.jpg'
+import img_mobile3 from './assets/images/mobile-image-hero-3.jpg'
 import img_desktop1 from './assets/images/desktop-image-hero-1.jpg'
+import img_desktop2 from './assets/images/desktop-image-hero-2.jpg'
+import img_desktop3 from './assets/images/desktop-image-hero-3.jpg'
 import dark from './assets/images/image-about-dark.jpg'
+import data from './data.json'
+import {useState} from 'react'
+import logo from './assets/images/logo.svg'
 import light from './assets/images/image-about-light.jpg'
-//import hamburger from './assets/images/icon-hamburger.svg'
-//import close from './assets/images/icon-close.svg'
 import arrow from './assets/images/icon-arrow.svg'
 // import angle_left from './assets/images/icon-angle-left.svg'
 // import angle_right from './assets/images/icon-angle-right.svg'
 
 function App() {
 
+  // const [jsondata, setJsonData] = useState(data)
+  // const [index, setIndex] = useState(0)
+  const [drawer, setDrawer] = useState(false)
+
+  // const imgDesktop = [img_desktop1 , img_desktop2, img_desktop3]
+  // const imgMobile = [img_mobile1, img_mobile2, img_mobile3]
+
   return (
     <div className="App">
         <section className="hero">
-          <div className="hero--main-page relative">
+          <div className="hero--main-page">
             <picture>
               <source media="(min-width:1024px)" srcSet={img_desktop1}/>
               <img src={img_mobile1} alt="" className='hero--main-page--img'/>
             </picture>
-            <div>
-
+            <div className='hero--nav'>
+              <div className='hero--nav-menu'>
+                <button
+                className="relative w-10 h-10 text-gray-500 bg-gray rounded-sm focus:outline-none"
+                onClick={() => setDrawer(!drawer)}>
+                <span className="sr-only">Open main menu</span>
+                <div className="absolute  block w-5 transform -translate-x-1/2 -translate-y-1/2 left-1/2 top-1/2">
+                  <span
+                    aria-hidden="true"
+                    className={`block absolute h-0.5 w-5  transform transition duration-500 ease-in-out ${
+                      drawer ? "rotate-45 bg-gray-500" : "-translate-y-1.5 bg-white"
+                    }`}></span>
+                  <span
+                    aria-hidden="true"
+                    className={`block absolute  h-0.5 w-5 bg-white   transform transition duration-500 ease-in-out ${
+                      drawer && "opacity-0"
+                    }`}></span>
+                  <span
+                    aria-hidden="true"
+                    className={`block absolute  h-0.5 w-5  transform  transition duration-500 ease-in-out ${
+                      drawer ? "-rotate-45 bg-gray-500" : "translate-y-1.5 bg-white"
+                    }`}></span>
+                </div>
+                </button>
+              </div>
+              <img src={logo} alt='Logo' className='' />
+            </div>
+            <div aria-checked={!drawer} className="absolute top-0 md:hidden right-0  left-0 transition-transform duration-500 z-50 ease-in-out h-[22%] bg-white aria-checked:-translate-y-[100%] flex flex-col gap-y-7">
+              <ul className='flex w-full justify-end pt-[25px] px-6 text-Dark font-bold text-lg gap-6'>
+                  <li className='nav--items'><a href="#">home</a></li>
+                  <li className='nav--items'><a href="#">shop</a></li>
+                  <li className='nav--items'><a href="#">about</a></li>
+                  <li className='nav--items'><a href="#">contact</a></li>
+              </ul>
             </div>
 
           </div>
