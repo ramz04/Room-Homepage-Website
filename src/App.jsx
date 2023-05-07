531771044a0529d578223ebd6e7f521ffd5eb237
Abdul-Rahman Wunpini <55752850+ramz04@users.noreply.github.com>
2023-05-07 20:44:56 +0000

    Added Nav Menu Desktop Page

--- src/App.jsx
+++ src/App.jsx
@@ -25,7 +25,7 @@ function App() {
 
   const prevSlide = () => {
     const currentSlide = index === 0
-    const newSlide = currentSlide ? data.length - 1 : currentSlide - 1
+    const newSlide = currentSlide ? data.length - 1 : index - 1
     setIndex(newSlide)
   }
 
@@ -38,15 +38,15 @@ function App() {
 
 
   return (
-    <div className="App">
-        <section className="hero">
-          <div className="hero--main-page">
+    <div className="App md:h-screen md:flex md:flex-col">
+        <section className="hero md:flex md:basis-3/5">
+          <div className="hero--main-page md:basis-3/5">
             <picture>
               <source media="(min-width:1024px)" srcSet={imgDesktop[index]}/>
-              <img src={imgMobile[index]} alt="" className='hero--main-page--img'/>
+              <img src={imgMobile[index]} alt="" className='hero--main-page--img md:h-[450px] lg:h-full'/>
             </picture>
             <div className='hero--nav'>
-              <div className='hero--nav-menu'>
+              <div className='hero--nav-menu md:hidden'>
                 <button
                 className="relative w-10 h-10 text-gray-500 bg-gray rounded-sm focus:outline-none"
                 onClick={() => setDrawer(!drawer)}>
@@ -71,6 +71,14 @@ function App() {
                 </button>
               </div>
               <img src={logo} alt='Logo' className='' />
+              <div>
+              <ul className='hidden md:flex w-full justify-end py-[25px] px-6 text-white font-bold md:font-normal text-lg md:gap-12 gap-6'>
+                  <li className='nav--item relative'><a href="#">home</a></li>
+                  <li className='nav--item relative'><a href="#">shop</a></li>
+                  <li className='nav--item relative'><a href="#">about</a></li>
+                  <li className='nav--item relative'><a href="#">contact</a></li>
+              </ul>
+              </div>
             </div>
             <div aria-checked={!drawer} className="fixed aria-checked:-translate-y-[100%] inset-0 bg-black bg-opacity-75 ease-in-out transition-transform duration-400">
               <div aria-checked={!drawer} className="absolute top-0 md:hidden right-0  left-0 transition-transform duration-500 z-50 ease-in-out bg-white aria-checked:-translate-y-[100%] flex flex-col gap-y-7">
@@ -82,28 +90,32 @@ function App() {
                 </ul>
               </div>
             </div>
-            <div className='hero--slide'>
+            <div className='hero--slide md:hidden'>
                 <button onClick={prevSlide}><img src={angle_left} alt="Slider Left" className='hero--slide-item' /></button>
                 <button onClick={nextSlide}><img src={angle_right} alt="Slider Right" className='hero--slide-item'/></button>
             </div>
 
           </div>
-          <div className="hero--discover">
+          <div className="hero--discover md:basis-2/5 md:p-[6rem]">
             <h1>{data[index].header}</h1>
             <p>
               {data[index].content}
             </p>
-            <div className='hover:text-gray-500 group'>
+            <div className='hover:text-gray-500 hero--shop group'>
               Shop now
               <img src={arrow} alt='Arrow' className='group-hover:invert-[.50] group-hover:ml-[15px] transition-all duration-500 ease-in-out' />
+            </div>
+            <div className='hero--slide-desktop'>
+                <button  className='slider--btn' onClick={prevSlide}><svg width="14" height="24" xmlns="http://www.w3.org/2000/svg"><path d="M13 0L1 12l12 12" stroke="#FFF" fill="none"fillRule="evenodd"/></svg></button>
+                <button className='slider--btn'  onClick={nextSlide}><svg width="14" height="24" xmlns="http://www.w3.org/2000/svg"><path d="M1 0l12 12L1 24" stroke="#FFF" fill="none" fillRule="evenodd"/></svg></button>
             </div>
 
           </div>
         </section>
-        <section className="about">
-          <img className='about--img' src={dark} alt='About Dark' />
-          <div className='about--text'>
-            <h3>About our furniture</h3>
+        <section className="about md:basis-2/5">
+          <img src={dark} alt='About Dark'className='about--img md:w-full' />
+          <div className='about--text md:p-[4rem]'>
+            <h3 className=''>About our furniture</h3>
             <p>
             Our multifunctional collection blends design and function to suit your individual taste.
             Make each room unique, or pick a cohesive theme that best express your interests and what
@@ -112,7 +124,7 @@ function App() {
             </p>
 
           </div>
-          <img className='about--img' src={light} alt='About Light' />
+          <img className='about--img md:w-full' src={light} alt='About Light ' />
         </section>
     </div>
   )
